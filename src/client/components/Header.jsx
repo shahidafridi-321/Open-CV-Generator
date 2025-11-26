20e6eb57c85d5066c29ac169b20ee5601fe38085
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-11-26 22:17:03 +0500

    Add content to Header

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -3,9 +3,9 @@ import { Navbar } from "./Navbar";
 
 export const Header = () => {
 	return (
-		<header className="flex justify-center bg-[#f0e9df] h-20 rounded-2xl  items-center w-full px-6 py-4 m-0">
-			<div className="w-full">
-				<h1 className="text-4xl font-bold    ">Open CV</h1>
+		<header className="flex  bg-[#200e32] h-20 rounded-2xl  items-center w-full px-6 py-4 m-0">
+			<div className="w-full m-0">
+				<h1 className="text-4xl font-bold text-white">Open CV</h1>
 			</div>
 			<Navbar />
 		</header>
