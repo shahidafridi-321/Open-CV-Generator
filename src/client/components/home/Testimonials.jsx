8fe2a2cdec95e4e85cb43db4c8cdd49159d76138
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-11-28 19:52:16 +0500

    Create Testimonial section component for home page

---
+++ src/client/components/home/Testimonials.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { testimonials } from "../../../utils/testimonialsData.jsx";
+
+export const Testimonials = () => {
+	return (
+		<div className="w-full flex flex-col  items-start sm:flex-row pb-24 sm:items-center sm:justify-around">
+			{testimonials.map((testimonial) => (
+				<div
+					className="flex sm:flex-col gap-5 items-center justify-center mb-8 sm:mb-0"
+					key={testimonial.description}
+				>
+					{testimonial.icon}
+					<p className="text-xl font-bold leading-tight tracking-tight sm:text-center sm:text-lg md:text-xl lg:text-2xl text-wrapped ">
+						{testimonial.description}
+					</p>
+				</div>
+			))}
+		</div>
+	);
+};
