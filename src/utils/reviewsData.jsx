56f9acf9035edea8db04adc458f1c0f72a3ffc42
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-11-28 19:50:50 +0500

    Move Data to utils

---
+++ src/utils/reviewsData.jsx
@@ -0,0 +1,80 @@
+export const reviews = [
+	{
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				className="w-10 lg:w-12"
+			>
+				<path
+					d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+					fill="#4285F4"
+				></path>
+				<path
+					d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+					fill="#34A853"
+				></path>
+				<path
+					d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+					fill="#FBBC05"
+				></path>
+				<path
+					d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+					fill="#EA4335"
+				></path>
+				<path d="M1 1h22v22H1z" fill="none"></path>
+			</svg>
+		),
+		comment:
+			"I absolutely love this site. It has made a huge difference in my and so many of my friends' career paths. Super easy to edit and customize. ",
+		name: "Siobhan K. ",
+		source: "Google Review",
+	},
+	{
+		icon: (
+			<svg
+				viewBox="0 0 100 100"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				className="w-10 lg:w-12"
+			>
+				<rect width="100" height="100" rx="50" fill="#000032"></rect>
+				<path
+					d="M85.0378 40.6585H58.3178L50.0645 15.2285L41.7845 40.6602L15.0645 40.6335L36.7045 56.3668L28.4245 81.7718L50.0645 66.0652L71.6778 81.7718L63.4245 56.3668L85.0378 40.6585Z"
+					fill="#00B67A"
+				></path>
+				<path
+					d="M65.2811 62.1179L63.4245 56.3662L50.0645 66.0645L65.2811 62.1179Z"
+					fill="#005128"
+				></path>
+			</svg>
+		),
+		comment:
+			"Great option totally free of cost and without watermarks even for the basic service. [...] Easy format, looks clean and clear.",
+		name: "Isabel R.",
+		source: "Trustpilot Review",
+	},
+	{
+		icon: (
+			<svg
+				viewBox="0 0 100 100"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				className="w-10 lg:w-12"
+			>
+				<path
+					d="M50 0C36.7392 0 24.0215 5.26784 14.6447 14.6447C5.26784 24.0215 0 36.7392 0 50C0 63.2608 5.26784 75.9785 14.6447 85.3553C24.0215 94.7322 36.7392 100 50 100C63.2608 100 75.9785 94.7322 85.3553 85.3553C94.7322 75.9785 100 63.2608 100 50C100 36.7392 94.7322 24.0215 85.3553 14.6447C75.9785 5.26784 63.2608 0 50 0Z"
+					fill="#0288D1"
+				></path>
+				<path
+					d="M24.998 37.5H34.998V75H24.998V37.5ZM29.968 32.5H29.913C26.928 32.5 24.998 30.275 24.998 27.4975C24.998 24.66 26.988 22.5 30.0255 22.5C33.068 22.5 34.9405 24.66 34.998 27.4975C34.998 30.275 33.068 32.5 29.968 32.5ZM77.498 51.25C77.498 43.655 71.343 37.5 63.748 37.5C59.093 37.5 54.9855 39.82 52.498 43.36V37.5H42.498V75H52.498V55C52.498 50.8575 55.8555 47.5 59.998 47.5C64.1405 47.5 67.498 50.8575 67.498 55V75H77.498C77.498 75 77.498 52.3025 77.498 51.25Z"
+					fill="white"
+				></path>
+			</svg>
+		),
+		comment:
+			"[...] I recently tried FlowCV, and I'm amazed at how quickly it lets you craft a polished, modern CV - literally in just minutes. [...]",
+		name: "Vailhavi R.",
+		source: "LinkedIn Post",
+	},
+];
