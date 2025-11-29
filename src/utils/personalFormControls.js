bb1610f8937e1b804a95c38b76827d7df3ad26b2
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-11-30 00:06:51 +0500

    Create form controls for creating a dynamic form

---
+++ src/utils/personalFormControls.js
@@ -0,0 +1,41 @@
+export let personalFormControls = [{
+  name: "fullName",
+  label: "Full Name",
+  placeholder: "Enter your title, first and last name",
+  type: "text",
+  componentType:"input"
+},
+  {
+  name: "professionalTitle",
+  label: "Professional title",
+  placeholder: "Target Position or current role.",
+  type: "text",
+  componentType:"input"
+},{
+  name: "profilePhoto",
+  label: "Photo",
+  placeholder: "Images",
+  type: "file",
+  componentType:"input"
+},{
+  name: "userEmail",
+  label: "Email",
+  placeholder: "Enter email",
+  type: "email",
+  componentType:"input"
+  },
+  {
+  name: "userPhone",
+  label: "Phone",
+  placeholder: "Enter phone",
+  type: "number",
+  componentType:"input"
+  },
+  {
+  name: "location",
+  label: "Location",
+  placeholder: "City,Country",
+  type: "number",
+  componentType:"input"
+  }
+]
