be23fdbebbce4401e33f99c1d7baea2d4bae0867
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-11-30 00:04:25 +0500

    Fix broken link

--- src/client/components/Navbar.jsx
+++ src/client/components/Navbar.jsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
 					</li>
 				</ul>
 				<button className="flex bg-blue-950 h-10 lg:h-12  w-auto px-3.5 sm:px-5 lg:px-8 cursor-pointer appearance-none items-center justify-center rounded-lg border-none text-sm lg:text-base font-bold text-white  hover:opacity-80 lg:rounded-xl ">
-					<Link to="resume">Start Now</Link>
+					<Link to="/resumes">Start Now</Link>
 				</button>
 			</div>
 		</nav>
