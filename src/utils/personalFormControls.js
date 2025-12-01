c3258fa1963d1788e7c093936d94c09c3f50480d
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-01 12:33:12 +0500

    Fix typo

--- src/utils/personalFormControls.js
+++ src/utils/personalFormControls.js
@@ -1,41 +1,54 @@
-export let personalFormControls = [{
-  name: "fullName",
-  label: "Full Name",
-  placeholder: "Enter your title, first and last name",
-  type: "text",
-  componentType:"input"
-},
-  {
-  name: "professionalTitle",
-  label: "Professional title",
-  placeholder: "Target Position or current role.",
-  type: "text",
-  componentType:"input"
-},{
-  name: "profilePhoto",
-  label: "Photo",
-  placeholder: "Images",
-  type: "file",
-  componentType:"input"
-},{
-  name: "userEmail",
-  label: "Email",
-  placeholder: "Enter email",
-  type: "email",
-  componentType:"input"
+export let personalFormControls = [
+	{
+    headers: [
+    {
+				name: "profilePhoto",
+				label: "Photo",
+				placeholder: "Images",
+				type: "file",
+				componentType: "file",
+			},
+			{
+				name: "fullName",
+				label: "Full Name",
+				placeholder: "Enter your title, first and last name",
+				type: "text",
+				componentType: "input",
+			},
+			{
+				name: "professionalTitle",
+				label: "Professional title",
+				placeholder: "Target Position or current role.",
+				type: "text",
+				componentType: "input",
+			},
+			
+		],
   },
   {
-  name: "userPhone",
-  label: "Phone",
-  placeholder: "Enter phone",
-  type: "number",
-  componentType:"input"
-  },
-  {
-  name: "location",
-  label: "Location",
-  placeholder: "City,Country",
-  type: "text",
-  componentType:"input"
+    body: [
+      {
+		name: "email",
+		label: "Email",
+		placeholder: "Enter email",
+		type: "email",
+		componentType: "input",
+	},
+	{
+		name: "phone",
+		label: "Phone",
+		placeholder: "Enter phone",
+		type: "number",
+		componentType: "input",
+	},
+	{
+		name: "location",
+		label: "Location",
+		placeholder: "City,Country",
+		type: "text",
+		componentType: "input",
+	},
+
+    ]
   }
-]
+	];
