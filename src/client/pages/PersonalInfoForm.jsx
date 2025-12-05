f636eaba9aadb6262998e043f795f6db3fc909aa
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-05 18:13:32 +0500

    Create personalInfoForm component

---
+++ src/client/pages/PersonalInfoForm.jsx
@@ -0,0 +1,106 @@
+import React, { useRef, useState } from "react";
+
+export const PersonalInfoForm = ({
+	formData,
+	setFormData,
+	personalFormControls,
+}) => {
+	const fileInputRef = useRef(null);
+
+	const handleImageChange = (e) => {
+		const file = e.target.files[0];
+		console.log(file);
+	};
+
+	const handleDivClick = () => {
+		fileInputRef.current.click();
+	};
+
+	const handleInputField = (dataObject) => {
+		switch (dataObject.componentType) {
+			case "input":
+				return (
+					<div className="w-full flex flex-col gap-1 ">
+						<label
+							htmlFor={dataObject.name}
+							className="text-sm font-semibold text-gray-800"
+						>
+							{dataObject.label}
+						</label>
+
+						<input
+							type={dataObject.type}
+							placeholder={dataObject.placeholder}
+							className="w-full bg-gray-100 text-gray-700 rounded-lg outline-none px-4 py-3 shadow-sm border border-gray-200 focus:ring-2 focus:ring-blue-500"
+							value={formData.personalInfo[dataObject.name] || ""}
+							onChange={(e) => {
+								setFormData((prevData) => ({
+									...prevData,
+									personalInfo: {
+										...prevData.personalInfo,
+										[dataObject.name]: e.target.value,
+									},
+								}));
+							}}
+						/>
+					</div>
+				);
+
+			case "file":
+				return (
+					<div className="w-full flex flex-col items-center col-span-2 md:col-span-1">
+						<label className="text-sm font-semibold text-gray-800 mb-2">
+							{dataObject.label}
+						</label>
+
+						<div
+							onClick={handleDivClick}
+							className="w-[160px] h-[160px] rounded-full bg-gray-200 flex items-center justify-center cursor-pointer hover:bg-gray-300 transition"
+						>
+							<span className="text-gray-600 text-sm">Upload Photo</span>
+
+							<input
+								type="file"
+								ref={fileInputRef}
+								className="hidden"
+								onChange={handleImageChange}
+							/>
+						</div>
+					</div>
+				);
+		}
+	};
+
+	return (
+		<div className="w-full flex items-start justify-center py-10">
+			<div className="w-full bg-white rounded-xl p-8 space-y-6">
+				<form className="w-full flex flex-col gap-6 justify-center items-center">
+					{personalFormControls[0].headers.map((item, index) => (
+						<div key={index} className="w-full">
+							{item.componentType === "file" ? (
+								<div className="w-full">{handleInputField(item)}</div>
+							) : (
+								<div className="w-full">{handleInputField(item)}</div>
+							)}
+						</div>
+					))}
+				</form>
+				<form className="w-full flex flex-col gap-6 justify-center items-center">
+					{personalFormControls[1].body.map((item, index) => (
+						<div key={index} className="w-full">
+							<div className="w-full">{handleInputField(item)}</div>
+						</div>
+					))}
+					<button
+						onClick={(e) => {
+							e.preventDefault();
+							console.log(formData);
+						}}
+					>
+						submit
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+};
