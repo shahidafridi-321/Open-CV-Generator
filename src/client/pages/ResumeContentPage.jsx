f8960e45e040058a35c1a517b34acc9d3ed2ae6c
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-05 18:14:02 +0500

    Refine the component

--- src/client/pages/ResumeContentPage.jsx
+++ src/client/pages/ResumeContentPage.jsx
@@ -1,36 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
+import { PersonalInfoForm } from "./PersonalInfoForm";
+import { personalFormControls } from "../../utils/personalFormControls";
+
+const sections = [
+	"Profile",
+	"Education",
+	"Professional Experience",
+	"Skills",
+	"Projects",
+	"Languages",
+	"Awards",
+	"References",
+];
+
+const initialFormData = {
+	personalInfo: {
+		profilePhoto: "",
+		fullName: "",
+		professionalTitle: "",
+		email: "",
+		phone: "",
+		location: "",
+	},
+};
 
 export const ResumeContentPage = () => {
-	return (
-		<main className="w-full p-4 grid grid-cols-5 h-[80vh]">
-			{/* Sidebar */}
-			<div className="col-span-1 h-full hidden md:flex md:flex-col gap-6 rounded shadow bg-gray-100 p-4">
-				<nav className="w-full flex flex-col gap-2">
-					<button className="w-auto py-3 px-16 rounded font-bold bg-gray-300">
-						Personal Info
-					</button>
-					<button className="w-auto py-3 px-16 rounded font-bold text-black">
-						Work Experience
-					</button>
-					<button className="w-auto py-3 px-16 rounded font-bold text-black">
-						Education
-					</button>
-					<button className="w-auto py-3 px-16 rounded font-bold text-black">
-						Skills
-					</button>
-					<button className="w-auto py-3 px-16 rounded font-bold text-black">
-						Projects
-					</button>
-				</nav>
-			</div>
+	const [formData, setFormData] = useState(initialFormData);
+	const [selectedSections, setSelectedSections] = useState([
+		"Personal Information",
+	]);
+	const [showPopup, setShowPopup] = useState(false);
+	const [opensection, setOpensection] = useState(null);
 
-			{/* Main Content */}
-			<div className="col-span-5 md:col-span-4 flex flex-col ">
-				<div className="flex flex-col py-4 px-6 gap-3">
-					<h1 className="text-4xl font-bold">Resume Content</h1>
-					{/* Form content goes here */}
+	return (
+		<main className="w-full p-4 grid grid-cols-5">
+			{/* Sidebar */}
+			{!showPopup ? (
+				<>
+					<div className="col-span-5 md:col-span-2 h-full md:flex md:flex-col gap-6 rounded shadow bg-gray-100 p-4">
+						<div className="w-full flex flex-col gap-2">
+							{selectedSections.map((selectedSection, index) => (
+								<div className="w-full self-stretch" key={index}>
+									<button
+										className="py-3 px-16 rounded font-bold bg-gray-300"
+										onClick={() =>
+											setOpensection(
+												opensection === selectedSection ? null : selectedSection
+											)
+										}
+									>
+										{opensection === selectedSection ? "Hide" : "Edit"}{" "}
+										{selectedSection}
+									</button>
+									{opensection === "Personal Information" &&
+										selectedSection === "Personal Information" && (
+											<PersonalInfoForm
+												personalFormControls={personalFormControls}
+												formData={formData}
+												setFormData={setFormData}
+											/>
+										)}
+									{opensection === "Education" &&
+										selectedSection === "Education" && <p>education form</p>}
+								</div>
+							))}
+						</div>
+
+						<button
+							onClick={() => setShowPopup(!showPopup)}
+							className="bg-pink-500 hover:bg-pink-600 w-40 py-2 px-6 rounded text-gray-100 font-bold transition-all"
+						>
+							Add Section
+						</button>
+					</div>
+
+					{/* Main Content */}
+					<div className="col-span-5 md:col-span-3 flex flex-col ">
+						<div className="flex flex-col py-4 px-6 gap-3">
+							<h1 className="text-4xl font-bold">Resume Content</h1>
+							{/* Form content goes here */}
+							{JSON.stringify(formData)}
+						</div>
+					</div>
+				</>
+			) : (
+				<div className="w-full bg-gray-200  rounded-xl flex  flex-col gap-3 col-span-5 shadow-2xl p-10 ">
+					<div className="w-full flex flex-col space-y-3 md:flex-row justify-between md:items-center px-6 py-4">
+						<h2 className="text-4xl font-bold">Add Section</h2>
+						<button
+							className="bg-pink-500 hover:bg-pink-600 w-40 py-2 px-6 rounded text-gray-100 font-bold transition-all"
+							onClick={() => setShowPopup(false)}
+						>
+							Close
+						</button>
+					</div>
+					<div className="flex flex-wrap items-center gap-5">
+						{sections.map((section) => (
+							<div
+								key={section}
+								className="max-w-[350px] rounded-xl shadow px-6 py-4 bg-gray-100"
+								onClick={() => {
+									if (!selectedSections.includes(section)) {
+										setSelectedSections([...selectedSections, section]);
+									}
+									setShowPopup(false);
+								}}
+							>
+								<h2 className="text-xl font-bold">{section}</h2>
+								<p className="text-[16px] text-gray-400">
+									Make a great first impression by presenting yourself in a few
+									sentences.
+								</p>
+							</div>
+						))}
+					</div>
 				</div>
-			</div>
+			)}
 		</main>
 	);
 };
