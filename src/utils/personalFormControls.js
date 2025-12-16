939241f488e5f181a610d2a691ffdf212844f1a6
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-16 18:58:54 +0500

    add initialFormdata for personalInfoForm

--- src/utils/personalFormControls.js
+++ src/utils/personalFormControls.js
@@ -1,54 +1,58 @@
-export let personalFormControls = [
-	{
-    headers: [
-    {
-				name: "profilePhoto",
-				label: "Photo",
-				placeholder: "Images",
-				type: "file",
-				componentType: "file",
-			},
-			{
-				name: "fullName",
-				label: "Full Name",
-				placeholder: "Enter your title, first and last name",
-				type: "text",
-				componentType: "input",
-			},
-			{
-				name: "professionalTitle",
-				label: "Professional title",
-				placeholder: "Target Position or current role.",
-				type: "text",
-				componentType: "input",
-			},
-			
-		],
-  },
-  {
-    body: [
-      {
-		name: "email",
-		label: "Email",
-		placeholder: "Enter email",
-		type: "email",
-		componentType: "input",
-	},
-	{
-		name: "phone",
-		label: "Phone",
-		placeholder: "Enter phone",
-		type: "number",
-		componentType: "input",
-	},
-	{
-		name: "location",
-		label: "Location",
-		placeholder: "City,Country",
-		type: "text",
-		componentType: "input",
-	},
+export let personalFormControls = {
+	headers: [
+		{
+			name: "profilePhoto",
+			label: "Photo",
+			placeholder: "Images",
+			type: "file",
+			componentType: "file",
+		},
+		{
+			name: "fullName",
+			label: "Full Name",
+			placeholder: "Enter your title, first and last name",
+			type: "text",
+			componentType: "input",
+		},
+		{
+			name: "professionalTitle",
+			label: "Professional title",
+			placeholder: "Target Position or current role.",
+			type: "text",
+			componentType: "input",
+		},
+	],
 
-    ]
-  }
-	];
+	body: [
+		{
+			name: "email",
+			label: "Email",
+			placeholder: "Enter email",
+			type: "email",
+			componentType: "input",
+		},
+		{
+			name: "phone",
+			label: "Phone",
+			placeholder: "Enter phone",
+			type: "number",
+			componentType: "input",
+		},
+		{
+			name: "location",
+			label: "Location",
+			placeholder: "City,Country",
+			type: "text",
+			componentType: "input",
+		},
+	],
+};
+
+export const initalPersonalFormData = {
+	profilePhoto: "",
+	fullName: "",
+	professionalTitle: "",
+	email: "",
+	phone: "",
+	location: "",
+};
