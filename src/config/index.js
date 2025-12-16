c5ceba0000b272470fe9e781d8d4fb5936b810ff
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-16 23:08:40 +0500

    Create a general form controler file that store different form controlls

---
+++ src/config/index.js
@@ -0,0 +1,53 @@
+export const profileFormControls = [
+	{
+		name: "profile",
+		label: "Professional Profile",
+		placeholder:
+			"Write a professional summary or objective that highlights your key qualification and career goals.",
+		componentType: "textarea",
+		type: "text",
+	},
+];
+
+export const educationFormControls = [
+	{
+		name: "degree",
+		label: "Degree",
+		placeholder: "Enter Degree/Field of study",
+		componentType: "input",
+		type: "text",
+	},
+	{
+		name: "school",
+		label: "School",
+		placeholder: "Enter School/University",
+		componentType: "input",
+		type: "text",
+	},
+	{
+		name: "startYear",
+		label: "Start Year",
+		placeholder: "Enter Start Year",
+		componentType: "input",
+		type: "date",
+	},
+	{
+		name: "endYear",
+		label: "End Year",
+		placeholder: "Enter End Year",
+		componentType: "input",
+		type: "date",
+	},
+	{
+		name: "description",
+		label: "Description",
+		placeholder: "Enter Description",
+		componentType: "textarea",
+		type: "text",
+	},
+];
+export const initialProfileFormData = [
+	{
+		summary: "",
+	},
+];
