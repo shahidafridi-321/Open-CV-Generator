17e616272cc7884c255b331fe0f5b12d21cd76a5
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-18 07:18:13 +0500

    create a PopUp component that renders a set of sections to be selected for CV

---
+++ src/client/components/contentPage/PopUp.jsx
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+import { CVContext } from "../../context/CVContextProvider";
+const sections = [
+	"Profile",
+	"Education",
+	"Professional Experience",
+	"Skills",
+	"Projects",
+	"Languages",
+	"Awards",
+	"References",
+];
+
+export const PopUp = ({
+	selectedSections,
+	setSelectedSections,
+	setShowPopup,
+}) => {
+	const { setFinalFormData } = useContext(CVContext);
+	return (
+		<div className="w-full bg-gray-200  rounded-xl flex  flex-col gap-3 col-span-5 shadow-2xl p-10 ">
+			<div className="w-full flex flex-col space-y-3 md:flex-row justify-between md:items-center px-6 py-4">
+				<h2 className="text-4xl font-bold">Add Section</h2>
+				<button
+					className="bg-pink-500 hover:bg-pink-600 w-40 py-2 px-6 rounded text-gray-100 font-bold transition-all"
+					onClick={() => setShowPopup(false)}
+				>
+					Close
+				</button>
+			</div>
+			<div className="flex flex-wrap items-center gap-5">
+				{sections.map((section) => (
+					<div
+						key={section}
+						className="max-w-[350px] rounded-xl shadow px-6 py-4 bg-gray-100"
+						onClick={() => {
+							if (!selectedSections.includes(section)) {
+								setSelectedSections([...selectedSections, section]);
+								setFinalFormData((prevData) => ({
+									...prevData,
+									[section.toLowerCase()]: [],
+								}));
+							}
+							setShowPopup(false);
+						}}
+					>
+						<h2 className="text-xl font-bold">{section}</h2>
+						<p className="text-[16px] text-gray-400">
+							Make a great first impression by presenting yourself in a few
+							sentences.
+						</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
