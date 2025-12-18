88efa07079dff2062469fcaaefa10689721211c3
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-19 01:34:51 +0500

    use useMemo hook to avoid un-necessary renders

--- src/client/context/CVContextProvider.jsx
+++ src/client/context/CVContextProvider.jsx
@@ -1,11 +1,11 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useMemo, useState } from "react";
 export const CVContext = createContext();
 
 export const CVContextProvider = ({ children }) => {
 	const [finalFormData, setFinalFormData] = useState({});
-	return (
-		<CVContext value={{ finalFormData, setFinalFormData }}>
-			{children}
-		</CVContext>
+	const value = useMemo(
+		() => ({ finalFormData, setFinalFormData }),
+		[finalFormData]
 	);
+	return <CVContext value={value}>{children}</CVContext>;
 };
