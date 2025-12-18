e33a4182d4fb27898f5ffda48e19fb5765a20164
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-18 17:28:20 +0500

    add a general form renderer

--- src/client/pages/ResumeContentPage.jsx
+++ src/client/pages/ResumeContentPage.jsx
@@ -1,18 +1,8 @@
 import React, { useContext, useState } from "react";
-import { PersonalInfoForm } from "../forms/PersonalInfoForm";
 import { CVContext } from "../context/CVContextProvider";
-import { FormEngine } from "../forms/FormEngine";
-import {
-	awardsFormControls,
-	educationFormControls,
-	languagesFormControls,
-	professionalExperieceFormControls,
-	profileFormControls,
-	projectsFormControls,
-	referenciesFormControls,
-	skillsFormControls,
-} from "../../utils/config";
+
 import { PopUp } from "../components/contentPage/PopUp";
+import { SectionFormRenderer } from "../components/contentPage/SectionFormRenderer";
 
 export const ResumeContentPage = () => {
 	const { finalFormData } = useContext(CVContext);
@@ -43,64 +33,11 @@ export const ResumeContentPage = () => {
 										{opensection === selectedSection ? "Hide" : "Edit"}{" "}
 										{selectedSection}
 									</button>
-									{opensection === "Personal Information" &&
-										selectedSection === "Personal Information" && (
-											<PersonalInfoForm />
-										)}
-									{opensection === "Education" &&
-										selectedSection === "Education" && (
-											<FormEngine
-												formControls={educationFormControls}
-												sectionName={selectedSection.toLowerCase()}
-											/>
-										)}
-									{opensection === "Profile" &&
-										selectedSection === "Profile" && (
-											<FormEngine
-												formControls={profileFormControls}
-												sectionName={selectedSection.toLowerCase()}
-											/>
-										)}
-									{opensection === "Professional Experience" &&
-										selectedSection === "Professional Experience" && (
-											<FormEngine
-												formControls={professionalExperieceFormControls}
-												sectionName={selectedSection.toLowerCase()}
-											/>
-										)}
-									{opensection === "Skills" && selectedSection === "Skills" && (
-										<FormEngine
-											formControls={skillsFormControls}
-											sectionName={selectedSection.toLowerCase()}
-										/>
-									)}
-									{opensection === "Projects" &&
-										selectedSection === "Projects" && (
-											<FormEngine
-												formControls={projectsFormControls}
-												sectionName={selectedSection.toLowerCase()}
-											/>
-										)}
-									{opensection === "Languages" &&
-										selectedSection === "Languages" && (
-											<FormEngine
-												formControls={languagesFormControls}
-												sectionName={selectedSection.toLowerCase()}
-											/>
-										)}
-									{opensection === "Awards" && selectedSection === "Awards" && (
-										<FormEngine
-											formControls={awardsFormControls}
-											sectionName={selectedSection.toLowerCase()}
-										/>
-									)}
-									{opensection === "References" &&
-										selectedSection === "References" && (
-											<FormEngine
-												formControls={referenciesFormControls}
-												sectionName={selectedSection.toLowerCase()}
-											/>
-										)}
+
+									<SectionFormRenderer
+										selectedSection={selectedSection}
+										opensection={opensection}
+									/>
 								</div>
 							))}
 						</div>
