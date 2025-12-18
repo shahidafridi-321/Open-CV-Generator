41c9db9f64a441e094c3e50e0f1d769b0724f855
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-19 01:38:50 +0500

    create a separate file formControlsData

---
+++ src/utils/formControlsData.jsx
@@ -0,0 +1,21 @@
+import {
+	awardsFormControls,
+	educationFormControls,
+	languagesFormControls,
+	professionalExperieceFormControls,
+	profileFormControls,
+	projectsFormControls,
+	referenciesFormControls,
+	skillsFormControls,
+} from "./config/index";
+
+export const formControlsData = {
+	profile: profileFormControls,
+	education: educationFormControls,
+	professionalExperience: professionalExperieceFormControls,
+	skills: skillsFormControls,
+	projects: projectsFormControls,
+	languages: languagesFormControls,
+	awards: awardsFormControls,
+	references: referenciesFormControls,
+};
