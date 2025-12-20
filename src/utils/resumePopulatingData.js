e6845f4f38c20271351e12d7dc8afc734520746d
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-20 16:14:09 +0500

    refactor structure of data

--- src/utils/resumePopulatingData.js
+++ src/utils/resumePopulatingData.js
@@ -1,94 +1,93 @@
-export const defaultData = 
-  {
-    id: 1,
-    personalDetails: {
-      profilePhoto:"",
-      fullName: "Shahid Afridi",
-      professionalTitle: "Full Stack web dev",
-      email: "shahidafridisa321@gmail.com",
-      phone: "03201985766",
-      location: "peshawar,pakistan",
-      linkedIn: null,
-      website: null,
-      nationality: null,
-      dateOfBirth: null,
-      visaStatus: null,
-      idNumber: null,
-      gender:null
-      
-    },
-    profile: [
-      {
-        description: "i am a dev hah hghagd hjgaDNB AGD sjdh "
-      }
-    ],
-    education: [
-      {
-        degree: "Bs computer science",
-        school: "islamia college peshawar",
-        startDate: "01/01/2019",
-        endDate: "01/02/2021",
-        location: "peshawar,pakistan",
-        description:null
-      }
-    ],
-    professionalProfile: [
-      {
-        jobTitle: "data entry clerk",
-        employer: "al rahman",
-        startDate: "01/01/2019",
-        endDate: "01/02/2021",
-        location: "peshawar,pakistan",
-        description:null
-      }
-      
-    ],
-    skills: [
-      {
-        skil: 'Web dev',
-        level: "intermedaite",
-        
-      }
-    ]
-    ,
-    languages: [
-      {
-        language: "pashto",
-        level: "entry",
-      }
-    ],
-    certificates: [{
-      certificate: "Web dev",
-      level:'begainer'
-    }],
-    projects: [{
-      projectTitle: "online plateform",
-      subTitle: null,
-       startDate: "01/01/2019",
-        endDate: "01/02/2021",
-        location: "peshawar,pakistan",
-        description:null
-      
-    }],
-    awards: [
-      {
-        award: "prime minister",
-        issuer: "Goverment",
-         startDate: "01/01/2019",
-        endDate: "01/02/2021",
-        location: "peshawar,pakistan",
-        description:null
-        
-      }
-    ],
-    referencies: [
-      {
-        name: "shaukat ali",
-        jobTitle: "Dr.",
-        organization: "university",
-        email: "shaukatali@gmail.com",
-        phone:"02827894"
-      }
-    ]
-
-  }
+export const defaultData = {
+	personalInformation: {
+		profilePhoto: "",
+		fullName: "Shahid Afridi",
+		professionalTitle: "Full Stack web dev",
+		email: "shahidafridisa321@gmail.com",
+		phone: "03201985766",
+		location: "peshawar,pakistan",
+		linkedIn: null,
+		website: null,
+		nationality: null,
+		dateOfBirth: null,
+		visaStatus: null,
+		idNumber: null,
+		gender: null,
+	},
+	profile: [
+		{
+			description: "i am a dev hah hghagd hjgaDNB AGD sjdh hjjahjhsjhjshhjs ",
+		},
+	],
+	education: [
+		{
+			degree: "Bs computer science",
+			school: "islamia college peshawar",
+			startDate: "01/01/2019",
+			endDate: "01/02/2021",
+			location: "peshawar,pakistan",
+			description: null,
+		},
+	],
+	professionalProfile: [
+		{
+			jobTitle: "data entry clerk",
+			employer: "al rahman",
+			startDate: "01/01/2019",
+			endDate: "01/02/2021",
+			location: "peshawar,pakistan",
+			description: null,
+		},
+	],
+	skills: [
+		{
+			skil: "Web dev",
+			skilLevel: "intermedaite",
+		},
+	],
+	languages: [
+		{
+			language: "pashto",
+			languageLevel: "entry",
+		},
+		{
+			language: "urdu",
+			languageLevel: "entry",
+		},
+	],
+	certificates: [
+		{
+			certificate: "Web dev",
+			level: "begainer",
+		},
+	],
+	projects: [
+		{
+			projectTitle: "online plateform",
+			subTitle: null,
+			startDate: "01/01/2019",
+			endDate: "01/02/2021",
+			location: "peshawar,pakistan",
+			description: null,
+		},
+	],
+	awards: [
+		{
+			award: "prime minister",
+			issuer: "Goverment",
+			startDate: "01/01/2019",
+			endDate: "01/02/2021",
+			location: "peshawar,pakistan",
+			description: null,
+		},
+	],
+	referencies: [
+		{
+			name: "shaukat ali",
+			jobTitle: "Dr.",
+			organization: "university",
+			email: "shaukatali@gmail.com",
+			phone: "02827894",
+		},
+	],
+};
