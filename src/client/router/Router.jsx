95b7c085d39c89af1f4054810bde4893e03e6a01
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-20 16:15:28 +0500

    fix  un-expected ui

--- src/client/router/Router.jsx
+++ src/client/router/Router.jsx
@@ -43,7 +43,7 @@ export const routes = createBrowserRouter([
 				element: (
 					<CVContextProvider>
 						<FormDataContextProvider>
-							<ResumeContentPage />;
+							<ResumeContentPage />
 						</FormDataContextProvider>
 					</CVContextProvider>
 				),
