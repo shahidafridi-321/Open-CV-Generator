6307bc9a6f9cae7758301d2c3b6f277e3b2b1b97
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-22 00:31:25 +0500

    add an icon with profile title

--- src/client/templateEngine/sectionRenderers/TextSection.jsx
+++ src/client/templateEngine/sectionRenderers/TextSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { CgProfile } from "react-icons/cg";
 
 export const TextSection = ({ data, title }) => {
 	if (!data || data.length === 0) return null;
 	return (
 		<section className="w-full flex flex-col space-y-3 ">
 			<div className="w-full bg-[#283e4d] flex items-center justify-center py-1">
+				<CgProfile className="text-white mr-2"></CgProfile>
 				<h3 className=" text-white text-[13px] font-bold">{title}</h3>
 			</div>
 			{data.map((item, i) => (
