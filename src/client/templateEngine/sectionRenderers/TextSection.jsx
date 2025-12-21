f767156c87bfffd412ebe36911187fdc97110216
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-22 00:06:57 +0500

    handle if data is empty

--- src/client/templateEngine/sectionRenderers/TextSection.jsx
+++ src/client/templateEngine/sectionRenderers/TextSection.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 export const TextSection = ({ data, title }) => {
+	if (!data || data.length === 0) return null;
 	return (
-		<section className="w-full flex flex-col space-y-3 justify-center items-center">
+		<section className="w-full flex flex-col space-y-3 ">
 			<div className="w-full bg-[#283e4d] flex items-center justify-center py-1">
 				<h3 className=" text-white text-[13px] font-bold">{title}</h3>
 			</div>
 			{data.map((item, i) => (
-				<p className="w-full text-white text-[10px] no" key={i}>
+				<p className="w-full text-white text-[13px] leading-relaxed" key={i}>
 					{item.description}
 				</p>
 			))}
