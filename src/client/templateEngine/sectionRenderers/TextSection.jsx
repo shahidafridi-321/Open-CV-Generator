59ad0b66608b0668fcf6ad13a46a7d3ddb906739
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-21 22:47:11 +0500

    add jsx to render text sections data

--- src/client/templateEngine/sectionRenderers/TextSection.jsx
+++ src/client/templateEngine/sectionRenderers/TextSection.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const TextSection = ({ data, title }) => {
-	return <div>TextSection</div>;
+	return (
+		<section className="w-full flex flex-col space-y-3 justify-center items-center">
+			<div className="w-full bg-[#283e4d] flex items-center justify-center py-1">
+				<h3 className=" text-white text-[13px] font-bold">{title}</h3>
+			</div>
+			{data.map((item, i) => (
+				<p className="w-full text-white text-[10px] no" key={i}>
+					{item.description}
+				</p>
+			))}
+		</section>
+	);
 };
