efd7b754b3a8fe99db609f93a7a6fc86904ec1ce
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-22 00:05:26 +0500

    add logic to render jsx for different type of lists

--- src/client/templateEngine/sectionRenderers/ListSection.jsx
+++ src/client/templateEngine/sectionRenderers/ListSection.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import { Dots } from "./Dots";
 
 export const ListSection = ({ title, data, field }) => {
-	return <div>ListSection</div>;
+	if (!data || data.length === 0) return null;
+
+	return (
+		<section className="w-full flex flex-col space-y-3 ">
+			<div className="w-full bg-[#283e4d] flex items-center justify-center py-1">
+				<h3 className=" text-white text-[13px] font-bold">{title}</h3>
+			</div>
+			{field === "language" && (
+				<ul className="space-y-1">
+					{data.map((item, i) => (
+						<li
+							key={i}
+							className="flex justify-between items-center text-white text-[14px]"
+						>
+							<span>{item.language}</span>
+							<Dots level={item.level} />
+						</li>
+					))}
+				</ul>
+			)}
+			{field === "award" && (
+				<div className="space-y-2">
+					{data.map((item, i) => (
+						<div key={i}>
+							<h4 className="text-white text-[13px] font-bold">{item.award}</h4>
+							<p className="text-white text-[11px]">
+								{item.issuer} • {item.startDate} - {item.endDate}
+							</p>
+						</div>
+					))}
+				</div>
+			)}
+		</section>
+	);
 };
