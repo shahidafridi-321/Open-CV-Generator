c82b277c08388a139ae4cf186d3739456c8c2464
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-24 10:12:19 +0500

    fix a typo

--- src/client/templateEngine/sectionRenderers/TagsSection.jsx
+++ src/client/templateEngine/sectionRenderers/TagsSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export const TagsSection = ({ title, data }) => {
 	if (!data || data.length === 0) return null;
+
 	return (
 		<section className="w-full flex flex-col space-y-3 px-8">
 			<div className="w-full bg-[#eadfdf] flex items-center justify-center py-1">
@@ -11,7 +12,7 @@ export const TagsSection = ({ title, data }) => {
 				<ul className="space-y-1">
 					{data.map((item, i) => (
 						<li key={i} className="list-disc text-[#636363] text-[12px]">
-							{item.skil}
+							{item.skill} <span>{item.information}</span>
 						</li>
 					))}
 				</ul>
