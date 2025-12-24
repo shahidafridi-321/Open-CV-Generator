8ef1397706c6bdf3b5b5554220bbc2879e56bf73
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-24 17:10:51 +0500

    show edit and delete buttons for personalInfo button

--- src/client/components/contentPage/SectionFormRenderer.jsx
+++ src/client/components/contentPage/SectionFormRenderer.jsx
@@ -9,22 +9,35 @@ import { EditAndDelete } from "./EditAndDeleteEntryHandler";
 export const SectionFormRenderer = ({ opensection, selectedSection }) => {
 	const { finalFormData } = useContext(CVContext);
 	if (opensection !== selectedSection) return null;
-	const formControls = formControlsData[selectedSection];
-	const sectionData = finalFormData[selectedSection] || [];
+
+	const sectionData = finalFormData[selectedSection];
 
 	if (selectedSection === "personalInformation") {
-		return <PersonalInfoForm />;
+		return (
+			<>
+				<>
+					<PersonalInfoForm />
+					{sectionData && Object.keys(sectionData).length > 0 && (
+						<EditAndDelete sectionName="personalInformation" />
+					)}
+				</>
+			</>
+		);
 	}
 	return (
 		<>
-			<FormEngine formControls={formControls} sectionName={selectedSection} />
-			{sectionData.map((entry) => (
-				<EditAndDelete
-					key={entry.id}
-					entry={entry}
-					sectionName={selectedSection}
-				/>
-			))}
+			<FormEngine
+				formControls={formControlsData[selectedSection]}
+				sectionName={selectedSection}
+			/>
+			{Array.isArray(sectionData) &&
+				sectionData.map((entry) => (
+					<EditAndDelete
+						key={entry.id}
+						entry={entry}
+						sectionName={selectedSection}
+					/>
+				))}
 		</>
 	);
 };
