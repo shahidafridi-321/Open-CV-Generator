5d6fd2f7fc2291d03a66208ab3a71facd4efc093
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-24 10:14:30 +0500

    pass allCvs,and setter function  to context

--- src/client/context/CVContextProvider.jsx
+++ src/client/context/CVContextProvider.jsx
@@ -1,11 +1,21 @@
 import React, { createContext, useMemo, useState } from "react";
-export const CVContext = createContext();
+import { allCvs as CV } from "../../utils/config/allCvs";
+
+export const CVContext = createContext({
+	finalFormData: {},
+	setFinalFormData: () => {},
+	allCvs: [],
+	setAllCvs: () => {},
+});
 
 export const CVContextProvider = ({ children }) => {
 	const [finalFormData, setFinalFormData] = useState({});
+	const [allCvs, setAllCvs] = useState(CV);
+
 	const value = useMemo(
-		() => ({ finalFormData, setFinalFormData }),
-		[finalFormData]
+		() => ({ finalFormData, setFinalFormData, allCvs, setAllCvs }),
+		[finalFormData, allCvs]
 	);
-	return <CVContext value={value}>{children}</CVContext>;
+
+	return <CVContext.Provider value={value}>{children}</CVContext.Provider>;
 };
