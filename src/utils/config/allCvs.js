12ebd6ec99a08ca255897635fb3c7284c315afec
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-24 17:15:17 +0500

    simplify structure

--- src/utils/config/allCvs.js
+++ src/utils/config/allCvs.js
@@ -1,119 +1,117 @@
 export let allCvs = [
-	[
-		{
-			personalInformation: {
-				profilePhoto: "/home/shahid-afridi/Downloads/profile.jpg",
-				fullName: "Shahid Afridi",
-				professionalTitle: "Entry level web developer",
-				email: "shahidrahmansr87@gmail.com",
-				phone: "03201985766",
-				location: "Peshawar, Pakistsan",
-			},
-			profile: [
-				{
-					profile:
-						"Results-driven Computer Science graduate with hands-on expertise in full-stack web development using the MERN\nstack . Engineered a responsive online learning platform, course management, and Markdown integration. Proficient\nin React, Node.js and RESTful API, eager to deliver innovative web solutions in an entry-level role with a focus on\nperformance optimization and cybersecurity basics.",
-					id: "4fd69c63-eb9b-4c93-ba7f-0c0548b26456",
-				},
-			],
-			professionalExperience: [
-				{
-					jobTitle: "Data Entry Clerk",
-					employer: "Al Rahman Medical Center",
-					startDate: "2025-12-15",
-					endDate: "2025-10-15",
-					location: "Peshawar, Pakistsan",
-					description:
-						"Managed 50+ daily customer and stock records with 100% accuracy using MS Office,\nensuring data integrity for high-volume workflows.\nDeveloped automated scripts, boosting efficiency by 20%, a skill transferable to API data\nhandling and low-code platform development.",
-					id: "4f654758-9627-4995-acd4-005d3afdea10",
-				},
-			],
-			education: [
-				{
-					degree: "BS Computer Science",
-					school: "Islamia College Peshawar",
-					startDate: "2021-07-20",
-					endDate: "2025-12-25",
-					description:
-						"Islamia college university Peshawar\nExcelled in programming, databases, and web development, mastering MERN stack and\ncloud computing fundamentals.\nBuilt a responsive online learning platform with AI-driven features, achieving 99% uptime\nand supporting 100+ simulated users.",
-					id: "a2c7d839-112e-4f27-b606-2546df3bb41f",
-				},
-				{
-					degree: "Diploma in Information Technology (DIT)",
-					school: "Technical Board Peshawar",
-					startDate: "2025-12-19",
-					endDate: "2026-01-01",
-					description:
-						"echnical board Peshawar\nGained expertise in computer fundamentals, software tools, and data management, laying\nthe groundwork for prompt engineering and web development.",
-					id: "2e8127b1-e27e-4436-96df-3fa11c52a905",
-				},
-			],
-			skills: [
-				{
-					skill: "Front End:",
-					information:
-						"HTML, CSS, JavaScript, React, Tailwind CSS, Responsive Design.",
-					skillLevel: "Proficient",
-					id: "493a1114-7383-42d6-96e6-efd9e97c2419",
-				},
-				{
-					skill: "Back End:",
-					information: "Node.js, Express, MongoDB, RESTful APIs.",
-					skillLevel: "Proficient",
-					id: "86eb55b2-77f0-4d83-8372-e5d16388f9e4",
-				},
-				{
-					skill: "Tools & Fundamentals:",
-					information:
-						"VS Code, GitHub, Postman, Chrome DevTools, Vite, Computer Fundamentals, Data Entry/MS Office.",
-					skillLevel: "Amatuer",
-					id: "95e2e6f6-7af8-420b-9332-487b3e210476",
-				},
-			],
-			projects: [
-				{
-					projectTitle: "Online learning platform",
-					startDate: "2025-12-26",
-					endDate: "2025-12-18",
-					description:
-						"Developed a full-stack virtual university platform using React for the frontend,\nNode.js/Express for backend APIs, and MongoDB for data storage. Integrated Tailwind CSS\nfor responsive UI and GitHub for Markdown-based course content management. Achieved\nseamless navigation across Home, About, Courses, and Contact pages, with features for\nvideo lessons and user updates.",
-					id: "a894cb8c-f890-41c8-b941-bdd27aaf14bd",
-					subTitle: "A Virtual University",
-				},
-			],
-			languages: [
-				{
-					language: "Pashto",
-					languageLevel: "Proficient",
-					id: "eba1a796-8842-4f96-a52f-3eeaa12dbdd4",
-				},
-				{
-					language: "Urdu",
-					languageLevel: "Proficient",
-					id: "8af53a5d-51cd-4ba5-9c3f-22a0550f45b7",
-				},
-				{
-					language: "English",
-					languageLevel: "Fluent",
-					id: "7edac6aa-069f-4f48-84f8-492fb57a85a1",
-				},
-			],
-			awards: [
-				{
-					award: "Prime minister youth laptop scheme 2023",
-					issuer: "Goverment",
-					date: "2025-12-26",
-					description:
-						"Awarded for academic excellence in technology, providing tools to advance web\ndevelopment skills.",
-					id: "f3e69ae6-a0f1-4352-9729-71cd0a2cbbfe",
-				},
-			],
-			references: [
-				{
-					name: "Available Upon Request",
-					id: "0b9412fc-4fb4-4779-8eec-3e151d896b42",
-				},
-			],
+	{
+		personalInformation: {
+			profilePhoto: "/home/shahid-afridi/Downloads/profile.jpg",
+			fullName: "Shahid Afridi",
+			professionalTitle: "Entry level web developer",
+			email: "shahidrahmansr87@gmail.com",
+			phone: "03201985766",
+			location: "Peshawar, Pakistsan",
 		},
-	],
+		profile: [
+			{
+				profile:
+					"Results-driven Computer Science graduate with hands-on expertise in full-stack web development using the MERN\nstack . Engineered a responsive online learning platform, course management, and Markdown integration. Proficient\nin React, Node.js and RESTful API, eager to deliver innovative web solutions in an entry-level role with a focus on\nperformance optimization and cybersecurity basics.",
+				id: "4fd69c63-eb9b-4c93-ba7f-0c0548b26456",
+			},
+		],
+		professionalExperience: [
+			{
+				jobTitle: "Data Entry Clerk",
+				employer: "Al Rahman Medical Center",
+				startDate: "2025-12-15",
+				endDate: "2025-10-15",
+				location: "Peshawar, Pakistsan",
+				description:
+					"Managed 50+ daily customer and stock records with 100% accuracy using MS Office,\nensuring data integrity for high-volume workflows.\nDeveloped automated scripts, boosting efficiency by 20%, a skill transferable to API data\nhandling and low-code platform development.",
+				id: "4f654758-9627-4995-acd4-005d3afdea10",
+			},
+		],
+		education: [
+			{
+				degree: "BS Computer Science",
+				school: "Islamia College Peshawar",
+				startDate: "2021-07-20",
+				endDate: "2025-12-25",
+				description:
+					"Islamia college university Peshawar\nExcelled in programming, databases, and web development, mastering MERN stack and\ncloud computing fundamentals.\nBuilt a responsive online learning platform with AI-driven features, achieving 99% uptime\nand supporting 100+ simulated users.",
+				id: "a2c7d839-112e-4f27-b606-2546df3bb41f",
+			},
+			{
+				degree: "Diploma in Information Technology (DIT)",
+				school: "Technical Board Peshawar",
+				startDate: "2025-12-19",
+				endDate: "2026-01-01",
+				description:
+					"echnical board Peshawar\nGained expertise in computer fundamentals, software tools, and data management, laying\nthe groundwork for prompt engineering and web development.",
+				id: "2e8127b1-e27e-4436-96df-3fa11c52a905",
+			},
+		],
+		skills: [
+			{
+				skill: "Front End:",
+				information:
+					"HTML, CSS, JavaScript, React, Tailwind CSS, Responsive Design.",
+				skillLevel: "Proficient",
+				id: "493a1114-7383-42d6-96e6-efd9e97c2419",
+			},
+			{
+				skill: "Back End:",
+				information: "Node.js, Express, MongoDB, RESTful APIs.",
+				skillLevel: "Proficient",
+				id: "86eb55b2-77f0-4d83-8372-e5d16388f9e4",
+			},
+			{
+				skill: "Tools & Fundamentals:",
+				information:
+					"VS Code, GitHub, Postman, Chrome DevTools, Vite, Computer Fundamentals, Data Entry/MS Office.",
+				skillLevel: "Amatuer",
+				id: "95e2e6f6-7af8-420b-9332-487b3e210476",
+			},
+		],
+		projects: [
+			{
+				projectTitle: "Online learning platform",
+				startDate: "2025-12-26",
+				endDate: "2025-12-18",
+				description:
+					"Developed a full-stack virtual university platform using React for the frontend,\nNode.js/Express for backend APIs, and MongoDB for data storage. Integrated Tailwind CSS\nfor responsive UI and GitHub for Markdown-based course content management. Achieved\nseamless navigation across Home, About, Courses, and Contact pages, with features for\nvideo lessons and user updates.",
+				id: "a894cb8c-f890-41c8-b941-bdd27aaf14bd",
+				subTitle: "A Virtual University",
+			},
+		],
+		languages: [
+			{
+				language: "Pashto",
+				languageLevel: "Proficient",
+				id: "eba1a796-8842-4f96-a52f-3eeaa12dbdd4",
+			},
+			{
+				language: "Urdu",
+				languageLevel: "Proficient",
+				id: "8af53a5d-51cd-4ba5-9c3f-22a0550f45b7",
+			},
+			{
+				language: "English",
+				languageLevel: "Fluent",
+				id: "7edac6aa-069f-4f48-84f8-492fb57a85a1",
+			},
+		],
+		awards: [
+			{
+				award: "Prime minister youth laptop scheme 2023",
+				issuer: "Goverment",
+				date: "2025-12-26",
+				description:
+					"Awarded for academic excellence in technology, providing tools to advance web\ndevelopment skills.",
+				id: "f3e69ae6-a0f1-4352-9729-71cd0a2cbbfe",
+			},
+		],
+		references: [
+			{
+				name: "Available Upon Request",
+				id: "0b9412fc-4fb4-4779-8eec-3e151d896b42",
+			},
+		],
+	},
 ];
