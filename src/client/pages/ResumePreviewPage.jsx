c279a8a77be536d837f2430650b13b99901b1327
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-25 10:49:08 +0500

    remove paasing data through react router navigator function

--- src/client/pages/ResumePreviewPage.jsx
+++ src/client/pages/ResumePreviewPage.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import { PreviewShell } from "../templateEngine/PreviewShell";
 import { TemplateRenderer } from "../templateEngine/TemplateRenderer";
 import { twoColumnsSidebar } from "../../utils/templates/twoColumnsSidebar";
-import { useLocation } from "react-router";
-
 export const ResumePreviewPage = () => {
-	const { state } = useLocation();
 	return (
 		<main className="w-full p-4">
 			<PreviewShell>
-				<TemplateRenderer template={twoColumnsSidebar} data={state} />
+				<TemplateRenderer template={twoColumnsSidebar} />
 			</PreviewShell>
 		</main>
 	);
