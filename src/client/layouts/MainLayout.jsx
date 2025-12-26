cdf4b1417cc782fd74cb10b214e621c09ec849d9
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-12-26 10:38:30 +0500

    add a gradirnt

--- src/client/layouts/MainLayout.jsx
+++ src/client/layouts/MainLayout.jsx
@@ -5,7 +5,13 @@ import { Outlet } from "react-router";
 
 export const MainLayout = () => {
 	return (
-		<div className="flex flex-col min-h-screen bg-[#f0eeeb] text-gray-900">
+		<div
+			className="flex flex-col min-h-screen  text-gray-900  inset-0 z-0"
+			style={{
+				background:
+					"radial-gradient(125% 125% at 50% 90%, #fff 40%, #475569 100%)",
+			}}
+		>
 			<div className="w-full sticky top-0 z-50 p-6">
 				<Header />
 			</div>
